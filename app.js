80675377970dfe479d82b5fc30c91381646ae71e
Paul Thomas <paulthomas2107@gmail.com>
2023-03-10 12:52:05 +0000

    More

--- app.js
+++ app.js
@@ -22,6 +22,47 @@ const addGo = (e) => {
   go = go === 'circle' ? 'cross' : 'circle';
   infoDisplay.textContent = 'It is now ' + go + "'s go.";
   e.target.removeEventListener('click', addGo);
+  checkScore();
+};
+
+const checkScore = () => {
+  const allSqaures = document.querySelectorAll('.square');
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allSqaures[cell].firstChild?.classList.contains('circle')
+    );
+    if (circleWins) {
+      infoDisplay.textContent = 'Circle Wins !';
+      allSqaures.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  winningCombos.forEach((array) => {
+    const crosseWins = array.every((cell) =>
+      allSqaures[cell].firstChild?.classList.contains('cross')
+    );
+    if (crosseWins) {
+      infoDisplay.textContent = 'Cross Wins !';
+      allSqaures.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
 };
 
 createBoard();
